d1a7885eec9e02c902bb897cf1014daed8d46f05
Geraldo Delamore <gdelamore@hotmail.com>
2025-08-16 15:22:20 -0300

    update nft-transfer.js

--- transfer-nft.js
+++ transfer-nft.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const { ethers } = require('ethers');
 const contractABI = require('./ERC1155_ABI.json'); // Make sure this file exists with your ABI
 
-// Use your existing .env variable names
-const provider = new ethers.JsonRpcProvider(process.env.RPC_URL); // <-- You'll still need to add this one!
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const contractAddress = process.env.NFT_Contract_Address;
 const tokenId = process.env.TOKEN_ID;
@@ -11,26 +10,23 @@ const tokenId = process.env.TOKEN_ID;
 const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
 async function transferNFT(to, amount = 1) {
-  // `from` is wallet.address (sender), `to` is recipient, `id` is tokenId, `amount` is number of NFTs, `data` is empty by default
-  const tx = await contract.safeTransferFrom(
-    wallet.address,
-    to,
-    tokenId,
-    amount,
-    "0x"
-  );
-  await tx.wait();
-  return tx.hash;
+  console.log('Attempting NFT transfer:', { from: wallet.address, to, tokenId, amount });
+  try {
+    const tx = await contract.safeTransferFrom(
+      wallet.address,
+      to,
+      tokenId,
+      amount,
+      "0x"
+    );
+    console.log('Transaction sent, hash:', tx.hash);
+    await tx.wait();
+    console.log('Transaction confirmed, hash:', tx.hash);
+    return tx.hash;
+  } catch (err) {
+    console.error('NFT transfer failed:', err);
+    throw err;
+  }
 }
-
-// Example usage (for testing):
-// (async () => {
-//   try {
-//     const txHash = await transferNFT('0xRecipientAddress', 1);
-//     console.log('NFT transferred, tx:', txHash);
-//   } catch (err) {
-//     console.error('Transfer error:', err);
-//   }
-// })();
 
 module.exports = { transferNFT };
